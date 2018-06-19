aea636d164c1e7dcfd7c99139f3e7ccb93d57e5c
damilolo <adiodamilola@yahoo.com>
2018-06-19 14:10:28 -0700

    define handleChange and handleSubmit methods in signup.js file

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+	constructor() {
+		super();
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleSubmit() {
+
+	}
+
+	handleChange() {
+
+	}
+
 	render() {
 		return (
-			<div id="body">
+			<div id="body" onSubmit={this.handleSubmit}>
 				<form className="signup-form">
 					<div className="insta-logo-type"></div>
-					<input type="text" name="username" className="text-field username" placeholder="Username"/>
-					<input type="password" name="password" className="text-field password" placeholder="Password"/>
+					<input onChange={this.handleChange} type="text" name="username" className="text-field username" placeholder="Username"/>
+					<input onChange={this.handleChange} type="password" name="password" className="text-field password" placeholder="Password"/>
 					<input type="submit" name="signup" className="def-button signup" value="Sign up"/>
 				</form>
 				<div className="form-pointer">
