fcd00170f6f02ea43c049bb71d5a786eb3ebf4ab
damilolo <adiodamilola@yahoo.com>
2018-06-21 17:25:42 -0700

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+	return(
+		<div id="top">
+			<div className="topbar clearfix">
+				<a href="index.html">
+					<ul className="brands clearfix">
+						<li className="insta-logo"></li>
+						<li className="insta-logo-type"></li>
+					</ul>
+				</a>
+				<input type="text" className="text-field search" placeholder="Search"/>
+				<ul className="links">
+					<li className="link explore-icon explore"></li>
+					<li className="link notifications">
+						<div className="like-icon"></div>
+					</li>
+					<a href="profilepage.html"><li className="link user-icon profile"></li></a>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
